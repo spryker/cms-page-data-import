ef3cb1b2b9ef0aa47450e51b2bbbcabd1b4e7cc0
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Zed/CmsPageDataImport/CmsPageDataImportConfig.php
+++ src/Spryker/Zed/CmsPageDataImport/CmsPageDataImportConfig.php
@@ -13,11 +13,15 @@ use Spryker\Zed\DataImport\DataImportConfig;
 class CmsPageDataImportConfig extends DataImportConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const IMPORT_TYPE_CMS_PAGE = 'cms-page';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const IMPORT_TYPE_CMS_PAGE_STORE = 'cms-page-store';
